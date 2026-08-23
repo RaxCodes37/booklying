5eccf5b66d3b5cc8cbd8fe42d4149dc94c7c778c
Raxon Suri <121737678+RaxPlay@users.noreply.github.com>
2026-08-22 23:38:46 -0400

    Fix

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "drizzle-kit"
 
 export default defineConfig({
 	out: "./drizzle",
-	schema: "./app/schema.ts",
+	schema: "./auth-schema.ts",
 	dialect: "postgresql",
 	dbCredentials: {
 		url: process.env.DATABASE_URL!
