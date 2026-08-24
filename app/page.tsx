62e09a9f9c4fd8f1a8cd818670080019c8581da1
Raxon Suri <121737678+RaxPlay@users.noreply.github.com>
2026-08-23 21:38:47 -0400

    Routing and authorization

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
-export default function Home() {
+import { getSession } from "@/lib/auth";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const session = await getSession();
+
+  if(!session) redirect("/sign-up")
+
   return (
     <div>
       
