d1045176cd3bb4ab22d902eb8ead217f4c5504f2
Raxon Suri <121737678+RaxPlay@users.noreply.github.com>
2026-08-24 20:22:57 -0400

    Displayed services
    Not styled and booking functionality not added

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -1,7 +1,9 @@
-export default function page() {
+import BookingClient from "../components/pages/booking-page/book-client";
+
+export default function Booking() {
   return (
     <div>
-      
+      <BookingClient/>
     </div>
   )
 }
--- app/components/pages/booking-page/book-client.tsx
+++ app/components/pages/booking-page/book-client.tsx
@@ -1,9 +1,38 @@
-"use client"
+"use client";
 
-export default function BookClient() {
+import { services } from "@/utils/services";
+
+export default function BookingClient() {
   return (
-    <div>
-      
+    <div className="flex flex-col items-center" id="book">
+      <h1 className="text-4xl font-bold mt-10">
+        <span className="underline">Book</span> with <span>us</span>
+      </h1>
+
+      <h2 className="text-2xl font-bold mt-10">
+        Our <span>services</span>, Your <span className="underline">pick</span>
+      </h2>
+
+      <table className="border border-[#fc8787] w-90 text-center mt-5">
+        <thead>
+          <tr className="text-center p-2">
+            <th className="border border-[#fc8787]">Services</th>
+            <th className="p-2 border border-[#fc8787]">Book</th>
+          </tr>
+        </thead>
+        <tbody>
+          {services.map((service) => (
+            <tr className="border border-[#fc8787] p-2" key={service.serviceName}>
+              <td className="border border-[#fc8787] p-2">
+                {service.serviceName} - {service.serviceDuration}
+              </td>
+              <td >
+                <button>Book</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
