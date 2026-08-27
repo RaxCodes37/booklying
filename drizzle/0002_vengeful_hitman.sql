9681220c787fdc814fda3c56b44f33a609b5b684
Raxon Suri <121737678+RaxPlay@users.noreply.github.com>
2026-08-26 22:43:01 -0400

    Database migrations

---
+++ drizzle/0002_vengeful_hitman.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "appointments" ADD COLUMN "booked_time" varchar(5) NOT NULL;
---
+++ drizzle/0003_flimsy_hellfire_club.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "appointments" ALTER COLUMN "booked_user_name" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "appointments" ALTER COLUMN "booked_user_id" SET NOT NULL;
